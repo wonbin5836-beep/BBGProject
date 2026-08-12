70926c51492ea334ff2e69ebf836f957a422245b
wonbin5836 <wonbin5836@gmail.com>
2026-08-12 10:55:41 +0900

    Fix .gitignore wildcard patterns and remove build artifacts

--- Intermediate/Build/Win64/x64/BaseballGameProjectEditor/Development/Slate/SharedPCH.Slate.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Slate/Public/SlateSharedPCH.h
-#include "C:/Dev/un5/BaseballGameProject/Intermediate/Build/Win64/x64/BaseballGameProjectEditor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
-#include "Runtime/Slate/Public/SlateSharedPCH.h"
--- Intermediate/Build/Win64/x64/BaseballGameProjectEditor/Development/UnrealEd/SharedPCH.UnrealEd.RTTI.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
-#include "C:/Dev/un5/BaseballGameProject/Intermediate/Build/Win64/x64/BaseballGameProjectEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.RTTI.Cpp20.h"
-#include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
